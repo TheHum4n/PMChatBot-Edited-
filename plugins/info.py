79de3f75bfdcfa794a931195133a9c201365450e
@TheHuman <76818629+TheHum4n@users.noreply.github.com>
2021-08-18 22:18:31 +0300

    Update info.py

--- plugins/info.py
+++ plugins/info.py
@@ -10,7 +10,7 @@ else:
     from config import Config
 
 
-@Client.on_message(filters.private & filters.user(Config.ADMIN) & filters.command(['info']))
+@Client.on_message(filters.private & filters.user(Config.ADMIN) & filters.command(['bilgi']))
 async def user_info(bot, message):
     reference_id = True
     if message.reply_to_message is not None:
