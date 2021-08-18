9411308e5991fce1c387904aa29aba53d161d4ba
@TheHuman <76818629+TheHum4n@users.noreply.github.com>
2021-08-19 01:03:28 +0300

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -13,7 +13,7 @@ else:
     @Client.on_message(filters.private & filters.command(['help']))
 async def help_me(*bot, message):
     if message.from_user.id == Config.ADMIN:
-        bot = client
+        bot = bot + message
         return bot
     info = await bot.get_users(user_ids=message.from_user.id)
     await bot.send_message(
